d4b5992dd5e7c9af870b4a03a065ee8d4ad8dbc5
gogi999 <g.petrovic9@gmail>
2021-03-24 19:34:06 +0100

    Added ex09/src/index.js

---
+++ ex09/src/index.js
@@ -0,0 +1,73 @@
+// Lodash
+const _ = require('lodash');
+
+// only change code below this line
+// users nested array with four objects starts here
+const users = [
+    {
+        id: 1,
+        firstName: "John",
+        lastName: "Doe",
+        age: 24,
+        gender: "male"
+    },
+    {
+        id: 2,
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 5,
+        gender: "female"
+    },
+    {
+        id: 3,
+        firstName: "Jim",
+        lastName: "Carrey",
+        age: 54,
+        gender: "male"
+    },
+    {
+        id: 4,
+        firstName: "Kate",
+        lastName: "Winslet",
+        age: 40,
+        gender: "female"
+    }
+];
+// users nested array with four objects ends here
+
+// getUsers function - list of users starts here
+function getUsers() {
+    var output = "";
+    for (var i = 0; i < users.length; i++) {
+        [output] = [users[i]];
+        console.log(output);
+    }
+    return output;
+}
+// getUsers function - list of users ends here
+
+// findUserById(id) function starts here
+function findUserById(id) {
+    try {
+        // add appropriate code here
+        var user = _.find(users, { "id": id });
+        var iFindUser = `${user.id} - ${user.firstName} ${user.lastName} is ${user.age}, ${user.gender}`;
+
+        console.log(iFindUser);
+        return iFindUser;
+    } catch (error) {
+        error.message = "Cannot read property 'firstName' of undefined"
+        console.log(error.message); // Change this line
+        return error.message; // Change this line
+    }
+}
+// findUserById(id) function ends here   
+// Only change code above this line
+getUsers();
+findUserById(1); // Change this line
+findUserById(2); // Change this line
+findUserById(3); // Change this line
+findUserById(4); // Change this line
+findUserById(5); // Change this line
+
+module.exports = findUserById;
