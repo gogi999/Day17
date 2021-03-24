8b5bf15364cf1bf9c54c84b93e43a601f6ff705a
gogi999 <g.petrovic9@gmail>
2021-03-24 11:22:25 +0100

    Added ex02/src/index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,20 @@
+const MY_CAR = {
+    wheels: 4,
+    cylinders: 4,
+    size: 2.2
+};
+
+function main(myCar) {
+    // Only change code below this line
+
+    const { cylinders } = myCar;
+    const { size } = myCar; 
+    return {
+        cylinders,
+        size
+    };
+    // Only change code above this line
+}
+
+console.log(main(MY_CAR));
+module.exports = main;
