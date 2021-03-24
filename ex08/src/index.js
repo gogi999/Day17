1054d80b7ee5d3404ddc2a38e9bc52e63e00bd56
gogi999 <g.petrovic9@gmail>
2021-03-24 18:57:24 +0100

    Added ex08/src/index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,69 @@
+// Lodash
+const _ = require('lodash');
+
+// only change code below this line
+// users nested array with four objects starts here
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 24,
+        gender: "male"
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 5,
+        gender: "female"
+    },
+    {
+        firstName: "Jim",
+        lastName: "Carrey",
+        age: 54,
+        gender: "male"
+    },
+    {
+        firstName: "Kate",
+        lastName: "Winslet",
+        age: 40,
+        gender: "female"
+    }
+];
+// users nested array with four objects ends here
+
+// getUsers function - list of users starts here
+function getUsers() {
+    var output = "";
+    for (var i = 0; i < users.length; i++) {
+        [output] = [users[i]];
+        console.log(output);
+    }
+    return output;
+}
+// getUsers function - list of users ends here
+
+// findUser(lastName, gender) function starts here
+function findUser(lastName, gender) {
+    try {
+        // add appropriate code here
+        var user = _.find(users, { "lastName": lastName, "gender": gender });
+        var iFindUser = `${user.firstName} ${user.lastName} is ${user.age}, ${user.gender}`;
+
+        console.log(iFindUser);
+        return iFindUser;
+    } catch (error) {
+        error.message = "Cannot read property 'firstName' of undefined"
+        console.log(error.message); // Change this line
+        return error.message; // Change this line
+    }
+}
+// findUser(lastName, gender) function ends here   
+// Only change code above this line
+getUsers();
+findUser("Doe", "male"); // Change this line
+findUser("Doe", "female"); // Change this line
+findUser("Carrey", "male"); // Change this line
+findUser("Winslet", "female"); // Change this line
+findUser("Bowie", "male"); // Change this line
+
+module.exports = findUser;
